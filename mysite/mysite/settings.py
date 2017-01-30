301c63d0f12d04a05965d29c35f749feb59118ea
beast <halo2olah90@gmail.com>
2017-01-30 18:08:22 +0000

    Migration to postgres

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -83,11 +83,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = { 'default':
-    {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+DATABASES = { #'default':
+    #{
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'memoria',
+        'USER': 'beast',
+        'PASSWORD': open(os.path.join(BASE_DIR, 'pass.conf')).read(),
+        'HOST': '192.168.1.3',
+        'PORT': '',
     }
+
+
 }
 
 # Password validation
