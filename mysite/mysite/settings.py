490a0a609d69f15a7f5e7d1811506118a81ec74d
beast <halo2olah90@gmail.com>
2017-02-06 19:25:32 +0000

    Fix for Sites

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -29,7 +29,7 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = access_conf['Secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
